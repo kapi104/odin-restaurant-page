61b2b180c6b3552ec333c22ef3e9204213615f1e
kapi104 <skorupa7979@gmail.com>
2023-08-12 21:56:53 +0200

    created landing page, to do: create function in load.js to create full page

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-console.log('abcd');
+import './style.css';
+import {header, center, sideImg, sideText} from './load.js';
+import restaurant from './restaurant.jpg';
+import txt from './description.txt';
+
+console.log(restaurant);
+let content = document.querySelector('.content');
+let resImg = new Image()
+resImg.src = restaurant;
+console.log(resImg);
+
+content.appendChild(header());
+let centerDiv = center();
+content.appendChild(centerDiv);
+centerDiv.appendChild(sideImg()).appendChild(resImg);
+centerDiv.appendChild(sideText(txt));
